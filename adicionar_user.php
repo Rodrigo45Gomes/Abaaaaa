9fcfdde310e4a5ec1d2874c68d193d55b6c79ac2
rodrigo <rodrigo.gomes28599@al.aememmartins.pt>
2025-06-17 12:52:28 +0100

    commit

--- adicionar_user.php
+++ adicionar_user.php
@@ -10,19 +10,19 @@
     $servername= "localhost";
     $username="root";
     $password="";
-    $dbname="trabalho_final_t2(rg)";
+    $dbname="pap";
 
     $conn= new mysqli($servername, $username, $password, $dbname);
         if ($conn->connect_error) {
             die("connection failed: ". $conn-> connect_error);
         } else {
-            $add = "INSERT INTO users (userName, nome, userPass, userEmail, userNasc, genero) VALUES (\"".$_POST["user"]."\",\"".($_POST
-            ["nome"])."\",\"".$_POST["pass"]."\",\"".$_POST["mail"]."\",\"".$_POST["nasc"]."\",\"".$_POST["genero"]."\")";
+            $add = "INSERT INTO users (userName, userPass, userEmail, nome, nasc, genero) VALUES (\"".$_POST["userName"]."\",\"".($_POST
+            ["nome"])."\",\"".$_POST["userPass"]."\",\"".$_POST["userEmail"]."\",\"".$_POST["nasc"]."\",\"".$_POST["genero"]."\")";
             if($conn->query($add)===TRUE){
                 echo"</br></br></br><h2 align=center>Dados registados com sucesso!<h2/>";
                 echo "<meta http-equiv=\"refresh\" content=\"2; url=lista.php\">";
             } else{
-                echo "Erro; Não foi possível inserir dados na tabela Utilizadores ". $conn->error. "<p />";
+                echo "Erro; Não foi possível inserir dados na tabela users ". $conn->error. "<p />";
 
             }
             }
